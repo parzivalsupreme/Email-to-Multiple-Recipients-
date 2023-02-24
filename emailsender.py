be1a4dc8f33e50e10a773297a8bc5c414d517ffc
Elijah Caesar Macabenlar <65802000+parzivalsupreme@users.noreply.github.com>
2023-02-24 17:33:27 +0800

    Update emailsender.py

--- emailsender.py
+++ emailsender.py
@@ -8,21 +8,7 @@ email_recipients = ["ajah35961@gmail.com", "test35961e@gmail.com", "test35961e@g
 
 subject = "Boost Your Pet Service Business with Our Facebook Ads"
 
-body = """
-Hi there,
-
-My name is Elijah Vanderlinde of Vanderlinde Media, a company that specializes in helping pet service businesses advertise on Facebook.
-
-We understand the pet industry and can create an ad campaign that is tailored to your specific services and target audience. Our Facebook ads will help increase your revenue and bring more customers to your business.
-
-And the best part is, we only charge you if our ads bring in more money for your business.
-
-Let's schedule a consultation to discuss how we can take your pet service business to the next level. Please let me know if this is something that interests you and we can coordinate a time that works for you.
-
-Best,
-Elijah
-Vanderlinde Media
-"""
+body = """ Your Message"""
 
 context = ssl.create_default_context()
 
